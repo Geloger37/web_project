602d9bce3bad26840f4ba450e25dd4589fd2c8ca
Dakasha26 <39644476+Dakasha26@users.noreply.github.com>
2021-06-06 18:20:02 +0800

    Update auth.php

--- php/auth.php
+++ php/auth.php
@@ -1,18 +1,28 @@
 <?php
+    // ToDo: запустить базу на локальном
+    const HOST = 'localhost';
+    const USER = 'root';
+    const PASSWORD = 'root';
+    const DATABASE = 'Users';
+
+
+    $mysql = new mysqli(HOST, USER, PASSWORD, DATABASE);
+    if($mysqli->connect_errno){
+        // ToDO: Вывод ошибки, что не получается подключиться к БД
+    }
 
     $login = $_POST['login'];
     $password = $_POST['password'];
-    
-    $mysql = new mysqli('localhost', 'root', 'root', 'poisk_rukovoditelia');
-    
-    $result = $mysql->query("SELECT * FROM Users" WHERE 'login' = '$login' AND 'pasword' == '$password');
+
+    $query = 'SELECT * FROM ' . DATABASE . ' WHERE login = '. $login . ' AND password = ' . $password; 
+    $result = $mysql->query($query);
     $user = $result->fetch_assoc();
     
-    if(count($user) == 0) {
-        echo json_encode(0);
-        exit;
+    if(count($user) == 1){
+        // ToDo: если найден пользователь
+    } else{
+        // ToDo: информация о том, что пользователь не найден
     }
-    
-    echo json_encode(213);
 
+    $mysql->close();
 ?>
