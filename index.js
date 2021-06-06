1eb9fba790704d7ca369b8978f36096176b0f55d
Dakasha26 <39644476+Dakasha26@users.noreply.github.com>
2021-06-06 17:49:15 +0800

    Перенёс код в JS, работают кнопки

--- index.js
+++ index.js
@@ -1,62 +1,106 @@
-function get_rank() {
-    
-}
+const formButtons = 
+ `<form class="form-signin" id="form-buttons">
+    <button class="btn btn-lg btn-primary btn-block" type="button" id="repair">Восстановить пароль</button>
+    <h1 class="h3 mb-3 font-weight-normal">Регистрация</h1>
+    <button class="btn btn-lg btn-primary btn-block" type="button" id="student">Студент</button>
+    <button class="btn btn-lg btn-primary btn-block" type="button" id="boss">Руководитель</button>
+  </form>`;
+
+const formSignIn =
+ `<form class="form-signin" id="form-signin">
+    <h1 class="h3 mb-3 font-weight-normal">Авторизация</h1>
+    <label for="inputEmail" class="sr-only">Логин</label>
+    <input type="email" id="inputEmail" class="form-control" placeholder="Логин" required autofocus>
+    <label for="inputPassword" class="sr-only">Пароль</label>
+    <input type="password" id="inputPassword" class="form-control" placeholder="Пароль" required>
+    <button class="btn btn-lg btn-primary btn-block" type="submit" id="enter">Войти</button>
+  </form>`;
+
+const formStudentSingup = 
+ `<form class="form-signin" id="student-form-singup">
+    <h1 class="h3 mb-3 font-weight-normal">Регистрация студента</h1>
+    <label for="surname" class="sr-only">Фамилия</label>
+    <input type="text" id="surname" class="form-control" placeholder="Фамилия" required autofocus>
+    <label for="name" class="sr-only">Имя</label>
+    <input type="text" id="name" class="form-control" placeholder="Имя" required autofocus>
+    <label for="middle_name" class="sr-only">Отчество</label>
+    <input type="text" id="middle_name" class="form-control" placeholder="Отчество" autofocus>
+    <label for="campus" class="sr-only">Ссылка на кампус</label>
+    <input type="url" id="campus" class="form-control" placeholder="Ссылка на кампус" required autofocus>
+    <label for="inputEmail2" class="sr-only">Электронная почта</label>
+    <input type="email" id="inputEmail2" class="form-control" placeholder="Электронная почта" required autofocus>
+    <label for="inputPassword1" class="sr-only">Пароль</label>
+    <input type="password" id="inputPassword1" class="form-control" placeholder="Пароль" required>
+    <button class="btn btn-lg btn-primary btn-block" type="submit" id="student_signup">Зарегистриваться</button>
+  </form>`;
+
+const formRepair = 
+ `<form class="form-signin" id="form-repair">
+    <h1 class="h3 mb-3 font-weight-normal">Восстановление</h1>
+    <label for="inputEmail1" class="sr-only">Электронная почта</label>
+    <input type="email" id="inputEmail1" class="form-control" placeholder="Электронная почта" required autofocus>
+    <button class="btn btn-lg btn-primary btn-block" type="submit" id="enter">Отправить</button>
+  </form>`;
+
+const formBossSingup = 
+ `<form class="form-signin" id="boss-form-singup">
+    <h1 class="h3 mb-3 font-weight-normal">Регистрация руководителя</h1>
+    <label for="surname1" class="sr-only">Фамилия</label>
+    <input type="text" id="surname1" class="form-control" placeholder="Фамилия" required autofocus>
+    <label for="name1" class="sr-only">Имя</label>
+    <input type="text" id="name1" class="form-control" placeholder="Имя" required autofocus>
+    <label for="middle_name1" class="sr-only">Отчество</label>
+    <input type="text" id="middle_name1" class="form-control" placeholder="Отчество" autofocus>
+    <label for="inputEmail3" class="sr-only">Электронная почта</label>
+    <input type="email" id="inputEmail3" class="form-control" placeholder="Электронная почта" required autofocus>
+    <label for="places" class="sr-only">Количество свободных мест</label>
+    <input type="number" id="places" class="form-control" placeholder="Количество свободных мест" required autofocus>
+    <label for="inputPassword2" class="sr-only">Пароль</label>
+    <input type="password" id="inputPassword2" class="form-control" placeholder="Пароль" required>
+    <button class="btn btn-lg btn-primary btn-block" type="submit" id="boss_signup">Зарегистриваться</button>
+  </form>`;
+
 
 $(document).ready(() => {
-	$("#navbarDropdown").hide()
-    $("#form-repair").hide()
-    $("#student-form-singup").hide()
-    $("#boss-form-singup").hide()
-    
+  document.getElementById("root").innerHTML = formSignIn + formButtons;
 })
 
+$(document).on('click', '#repair', function () {
+  document.getElementById("root").innerHTML = formRepair;
+})
+
+$(document).on('click', '#student', function () {
+  document.getElementById("root").innerHTML = formStudentSingup;
+})
+
+$(document).on('click', '#boss', function () {
+  document.getElementById("root").innerHTML = formBossSingup;
+})
+
+
+// ToDo: доделать настройки
 $("#settings").click(() => {
     
 })
 
 $("#enter").click(() => {
-    $.post("auth.php",
-           {
-               login : $.trim($("input[id='inputEmail']").val()),
-                pass : $.trim($("input[type='password']").val())
-           },
-           (data, status) => {
-               if(data != 0) {
-                   $("#form-signin").hide()
-                   $("#form-repair").hide()
-                   $("#student-form-singup").hide()
-                   $("#boss-form-singup").hide()
-               }
-           }
-    )
+  $.post("auth.php",
+        {
+          login : $.trim($("input[id='inputEmail']").val()),
+          pass : $.trim($("input[type='password']").val())
+        },
+        (data, status) => {
+          if(data != 0) {
+            $("#form-signin").hide()
+            $("#form-repair").hide()
+            $("#student-form-singup").hide()
+            $("#boss-form-singup").hide()
+          }
+        }
+  )
 })
 
-$("#repair").click(() => {
-    $("#form-repair").show()
-    $("#form-signin").hide()
-    $("#form-buttons").hide()
-    $("#student-form-singup").hide()
-    $("#boss-form-singup").hide()
-})
-
-$("#student").click(() => {
-    $("#navbarDropdown").hide()
-    $("#form-repair").hide()
-    $("#form-signin").hide()
-    $("#form-buttons").hide()
-    $("#boss-form-singup").hide()
-    $("#student-form-singup").show()
-})
-
-$("#boss").click(() => {
-    $("#navbarDropdown").hide()
-    $("#form-repair").hide()
-    $("#form-signin").hide()
-    $("#form-buttons").hide()
-    $("#boss-form-singup").hide()
-    $("#student-form-singup").show()
-})
-
+// ToDo: доделать авторизацию
 $("#student_signup").click(() => {
     $.post("signup_user.php",
            {
