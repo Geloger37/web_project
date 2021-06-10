8f9f4f6b3ea0d385cb117c3d89962a27933834c8
Geloger37 <69764552+Geloger37@users.noreply.github.com>
2021-06-10 22:29:11 +0800

    signup_user

--- js/index.js
+++ js/index.js
@@ -22,13 +22,13 @@ const formStudentSingup =
     <label for="surname" class="sr-only">Фамилия</label>
     <input type="text" id="surname" class="form-control" placeholder="Фамилия" required autofocus>
     <label for="name" class="sr-only">Имя</label>
-    <input type="text" id="name" class="form-control" placeholder="Имя" required autofocus>
+    <input type="text" id="name" class="form-control" placeholder="Имя" required >
     <label for="middle_name" class="sr-only">Отчество</label>
-    <input type="text" id="middle_name" class="form-control" placeholder="Отчество" autofocus>
+    <input type="text" id="middle_name" class="form-control" placeholder="Отчество" >
     <label for="campus" class="sr-only">Ссылка на кампус</label>
-    <input type="url" id="campus" class="form-control" placeholder="Ссылка на кампус" required autofocus>
+    <input type="url" id="campus" class="form-control" placeholder="Ссылка на кампус" required >
     <label for="inputEmail2" class="sr-only">Электронная почта</label>
-    <input type="email" id="inputEmail2" class="form-control" placeholder="Электронная почта" required autofocus>
+    <input type="email" id="inputEmail2" class="form-control" placeholder="Электронная почта" required >
     <label for="inputPassword1" class="sr-only">Пароль</label>
     <input type="password" id="inputPassword1" class="form-control" placeholder="Пароль" required>
     <button class="btn btn-lg btn-primary btn-block" type="submit" id="student_signup">Зарегистриваться</button>
@@ -68,8 +68,6 @@ $(document).ready(() => {
   
   $('#form-signin').submit(function (e) { 
     e.preventDefault();
-    
-    var ok = 1;
 
     $.ajax({
       async: false,
@@ -101,71 +99,55 @@ $(document).ready(() => {
     $("#root").html(formBossSingup);
   })
  
-  /*$('#enter').click( () => {
-
-    
-
-    var ok = 1;
-
+  // ToDo: доделать регистрацию
+  $('#student-form-singup').submit(function (e) { 
+    e.preventDefault();
+  
     $.ajax({
       async: false,
       type: "POST",
-      url: "php/auth.php",
+      url: "php/signup_user.php",
       data: {
-        login: 'admin',
-        pass: 'admin'
+        login: $('#inputEmail2').val().trim(),
+        pass:  $('#inputPassword1').val().trim()
       },
     })
     .done((message) => {
-      if(message == 0) {
-        alert('Неправильный логин или пароль!');
-      } else {
-        window.location.href = "/search.html";
-      }
+      
     });
     
-    
-    function change() {
-      ok = 0;
-    }
-
-    if( ok ) {
+    /*$.ajax({
+      type: "POST",
+      url: "php/student_signup.php",
+      data: {
+        login: $('#inputEmail2').val().trim(),
+        pass:  $('#inputPassword1').val().trim()
+      },
+    })
+    .done((message) => {
       
-      $(location).attr('href', '');
-    }
-    else {
-      alert('Неверный логин или пароль!');
-    }
-    
-  } )*/
+    });*/
 
-})
+    window.location.href = "/search.html";     
+  })
 
-
-// ToDo: доделать регистрацию
-$("#student_signup").click(() => {
-    $.post("php/signup_user.php",
-           {
-               login : $.trim($("input[id='inputEmail']").val()),
-                pass : $.trim($("input[type='password']").val())
-           },
-           (data, status) => {
-               
-           }
-    )
-    
-})
-
-
-$("#boss_signup").click(() => {
+  $("#boss_signup").click(() => {
     $.post("boss_signup.php",
            {
                login : $.trim($("input[id='inputEmail']").val()),
                 pass : $.trim($("input[type='password']").val())
            },
            (data, status) => {
-               
+
            }
     )
-    
+         
+  })
+
 })
+
+
+
+
+
+
--- php/signup_user.php
+++ php/signup_user.php
@@ -1,19 +1,39 @@
 <?php
 
-    $login = $_POST['login'];
-    $password = $_POST['password'];
+const HOST = 'localhost';
+const USER = 'root';
+const PASSWORD = 'root';
+const DATABASE = 'poisk_rukovoditelia';
 
-    $ps = password_hash($password, PASSWORD_DEFAULT);
 
-    const HOST = 'localhost';
-    const USER = 'root';
-    const PASSWORD = 'root';
-    const DATABASE = 'poisk_rukovoditelia';
+$mysql = new mysqli(HOST, USER, PASSWORD, DATABASE);
+if($mysql->connect_errno){
+    // ToDO: Вывод ошибки, что не получается подключиться к БД
+}
 
-    $mysql = new mysqli(HOST, USER, PASSWORD, DATABASE);
-    
-    $result = $mysql->query("INSERT INTO Users VALUES($login, $ps);");
-    
-    echo json_encode($result);
+echo json_encode($_POST);
+
+$login = $_POST['login'];
+$password = $_POST['pass'];
+
+$ps = password_hash($password, PASSWORD_DEFAULT);
+
+$query_check = 'SELECT * FROM Users WHERE login =' . '\'' . $login . '\'';
+
+
+/*
+$result = $mysql->query($query_check);
+$users = $result->fetch_assoc();
+
+if($users !== null) {
+    echo json_encode(array('message' => 'Пользователь с такой электронной почтой уже существует.', 'success' => 0));
+}
+
+setcookie($login, time() - 3600);
+
+$mysql->close();
+
+echo password_verify($password, $pwd['pasword']);
+*/
 
 ?>
