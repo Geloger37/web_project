d497eb77ec238977a8e9e8b980e8d103802d98cc
Azot360null <65645145+Azot360null@users.noreply.github.com>
2021-06-13 20:46:02 +0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,6 +1,9 @@
 const tableSearch = 
  `<form class="form-signin" id="boss-table">
     <h1 class="h3 mb-3 font-weight-normal">Список руководителей</h1>
+    <label for="searchBox" class="sr-only">Поиск</label>
+    <input type="text" id="searchBox" class="form-control" placeholder="Поиск тегов" required autofocus>
+    <button class="btn btn-lg btn-primary btn-block" type="submit" id="search_button">Погнали Блеать!</button>
     <table id="table-search" class="display" cellspacing="0" width="100%">
       <thead>
             <tr>
@@ -34,4 +37,20 @@ const tableSearch =
   $(document).ready(() => {
     $("#root").html(tableSearch);
     $('#table-search').DataTable();
-  })
+  });
+
+  $('#search_button').click(() => {
+     e.preventDefault();
+
+    $.ajax({
+      async: false,
+      type: "POST",
+      //url: "php/auth.php", Придумай URL по типу search.php  
+      data: {
+        tags: $('#searchBox').val().trim()
+      },
+    })
+    .done((message) => {
+      //Я тупой и немогу придумать как результат вводить в таблицу :(
+    });
+  });
