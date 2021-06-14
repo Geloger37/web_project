e69570839046b8b02392f1434bcb5d418c2a2839
Geloger37 <69764552+Geloger37@users.noreply.github.com>
2021-06-14 09:58:48 +0800

    Search

--- js/search.js
+++ js/search.js
@@ -1,9 +1,11 @@
 const tableSearch = 
- `<form class="form-signin" id="boss-table">
+ `<form class="form-signin" id="search_panel">
     <h1 class="h3 mb-3 font-weight-normal">Список руководителей</h1>
     <label for="searchBox" class="sr-only">Поиск</label>
-    <input type="text" id="searchBox" class="form-control" placeholder="Поиск тегов" required autofocus>
-    <button class="btn btn-lg btn-primary btn-block" type="submit" id="search_button">Погнали Блеать!</button>
+    <input type="text" id="searchBox" class="form-control" placeholder="Список тегов" autofocus>
+    <button class="btn btn-lg btn-primary btn-block" type="button" id="search_button">Поиск</button>
+  </form>
+  <form class="form-signin" id="boss-table">
     <table id="table-search" class="display" cellspacing="0" width="100%">
       <thead>
             <tr>
@@ -14,23 +16,7 @@ const tableSearch =
                 <th>Рейтинг</th>
             </tr>
         </thead>
-        <tbody>
-            <tr>
-                <th>Название темы</th>
-                <th>Студент</th>
-                <th>Год</th>
-                <th>Специальность</th>
-                <th>Рейтинг</th>
-            </tr>
-            <tr>
-                <th>Название темы</th>
-                <th>Студент</th>
-                <th>Год</th>
-                <th>Специальность</th>
-                <th>Рейтинг</th>
-            </tr>
-
-        </tbody>
+        
     </table>
   </form>`
 
@@ -47,10 +33,14 @@ const tableSearch =
       type: "POST",
       //url: "php/auth.php", Придумай URL по типу search.php  
       data: {
-        tags: $('#searchBox').val().trim()
+        tags: $('#searchBox').val().trim().split(',')
       },
     })
     .done((message) => {
       //Я тупой и немогу придумать как результат вводить в таблицу :(
     });
   });
+
+  function get_rating(major, minor) {
+    return 10*major + minor;
+  }
